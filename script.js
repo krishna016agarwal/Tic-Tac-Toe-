96ee4d08ee8a5449059b65be878e0cce1af49f2a
Krishna Agarwal <krishna016agrawal@gmail.com>
2024-12-13 07:43:33 +0530

    commited

--- script.js
+++ script.js
@@ -1,99 +1,108 @@
-let a = document.querySelector("#a")
-let b = document.querySelector("#b")
-let c = document.querySelector("#c")
-let d = document.querySelector("#d")
-let e = document.querySelector("#e")
-let f = document.querySelector("#f")
-let g = document.querySelector("#g")
-let h = document.querySelector("#h")
-let i = document.querySelector("#i")
-let z = document.querySelector("body")
-let j = document.querySelectorAll(".box")
-play();
+let a = document.querySelector("#a");
+let b = document.querySelector("#b");
+let c = document.querySelector("#c");
+let d = document.querySelector("#d");
+let e = document.querySelector("#e");
+let f = document.querySelector("#f");
+let g = document.querySelector("#g");
+let h = document.querySelector("#h");
+let i = document.querySelector("#i");
+let z = document.querySelector("body");
+let audioElement = document.getElementById("myAudio");
+let audioElement1 = document.getElementById("myAudio1");
+let j = document.querySelectorAll(".box");
 
-
-
-win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7],
-[2, 5, 8], [0, 4, 8], [2, 4, 6]];
-
+win = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+var checkdraw1 = [];
+var checkdraw = false;
 function play() {
-    n = true;
+  n = true;
 
-    j.forEach((box) => {
-        box.onclick = () => {
-            if (n === true) {
-                box.innerHTML = "O";
+  j.forEach((box) => {
+    box.onclick = () => {
+      audioElement.play();
+      z.innerHTML;
+      if (n === true) {
+        box.innerHTML = "O";
 
+        n = false;
+      } else if (n === false) {
+        box.innerHTML = "X";
 
-                n = false;
-            } else if (n === false) {
-                box.innerHTML = "X"
+        n = true;
+      }
+      box.disabled = true;
+      if (box.innerText == "O") {
+        box.style.color = "blue";
+      } else if (box.innerText == "X") {
+        box.style.color = "red";
+      }
+      box.style.fontSize = "xx-large";
+      winner();
 
-                n = true;
-            }
-            box.disabled = true;
-            if (box.innerText == "O") {
+      checkdraw1.push(box.id);
 
-                box.style.color = "blue";
-            } else if (box.innerText == "X") {
-                box.style.color = "red";
-            }
-            box.style.fontSize = "xx-large";
-            winner();
-
+      if (checkdraw1.length === 9) {
+        if (checkdraw == false) {
+          showdraw();
         }
-    })
+      }
+    };
+  });
 }
+
 x = document.querySelector(".newgame");
 x.addEventListener("click", () => {
-    window.location.reload();
-})
-
+  window.location.reload();
+});
 
 function winner() {
-
-    for (let i of win) {
-
-        let p1 = j[i[0]].innerText;
-        let p2 = j[i[1]].innerText;
-        let p3 = j[i[2]].innerText;
-        if (p1 != "" && p2 != "" && p3 != "") {
-            if (p1 === p2 && p2 === p3) {
-             
-                showwinner(p1);
-            }
-        }
+  for (let i of win) {
+    let p1 = j[i[0]].innerText;
+    let p2 = j[i[1]].innerText;
+    let p3 = j[i[2]].innerText;
+    if (p1 != "" && p2 != "" && p3 != "") {
+      if (p1 === p2 && p2 === p3) {
+        audioElement1.play();
+        checkdraw = true;
+        showwinner(p1);
+      }
     }
-
-
-
+  }
 }
 
-
 function showdraw() {
-    z.innerHTML = `<div id="klkl">
+  audioElement1.play();
+  z.innerHTML = `<div id="klkl">
     <p> DRAW</p><br><br>
     <div id="kkl">RESTART</div>
     </div> 
-    `
-    hhj = document.querySelector("#kkl");
-    hhj.addEventListener("click", () => {
-        window.location = "index.html";
-    })
+    `;
+  hhj = document.querySelector("#kkl");
+  hhj.addEventListener("click", () => {
+    window.location = "index.html";
+  });
 }
 function showwinner(show) {
 
-
-    z.innerHTML = `<div id="klkl">
+  z.innerHTML = `<div id="klkl">
     <p>${show} is the winner</p><br><br>
     <div id="kkl">RESTART</div>
     </div> 
-    `
-    hhj = document.querySelector("#kkl");
-    hhj.addEventListener("click", () => {
-        window.location = "index.html";
-    })
+    `;
+  hhj = document.querySelector("#kkl");
+  hhj.addEventListener("click", () => {
+    window.location = "index.html";
+  });
 }
 
-
-console.log("gj")
+play();
